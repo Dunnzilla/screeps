f9caceee71cd1b53ec0e0bacbf2a3d37b05ad837
Jason Dunn <attnjd@gmail.com>
2017-09-08 11:05:32 -0500

    Deposit E into Stores, too.

--- prod/role.harvester.js
+++ prod/role.harvester.js
@@ -3,37 +3,26 @@ var roleUpgrader = require('role.upgrader');
 
 var roleHarvester = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_CONTAINER ||
-                        structure.structureType == STRUCTURE_TOWER ||
-                        structure.structureType == STRUCTURE_STORAGE
-                        ) && structure.energy < structure.energyCapacity;
+    getNextEnergyStoringMechanism: function(creep) {
+        // Find closest of any of these
+        var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+                    structure.structureType == STRUCTURE_SPAWN ||
+                    structure.structureType == STRUCTURE_CONTAINER ||
+                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            } else {
-                // Nothing to transfer energy to.  So let's upgrade stuff.
-                roleUpgrader.run(creep);
-            }
+        });
+        // Last preferred: stores. If all other structures are full of energy, start stuffing it into stores.
+        if( ! target ) {
+            target = creep.room.findClosestByRange(FIND_STRUCTURES, {
+                filter: (s) => { return ( s.structureType == STRUCTURE_STORAGE && s.storeCapacity > _.sum(s.store)); }}
+            );
         }
+        return target;
     },
-    
-    
+
+    /** @param {Creep} creep **/
     runOptimized: function(creep) {
         if(creep.memory.transferring && creep.carry.energy == 0) {
             creep.memory.transferring = false;
@@ -47,14 +36,8 @@ var roleHarvester = {
         if( creep.memory.transferring ) {
             // TODO Save xfer ID and move to it
             // Game.creeps['Harvester_762db1'].pos.findClosestByRange(FIND_STRUCTURES, { filter: (s) => (s.structureType == STRUCTURE_EXTENSION || s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_TOWER });
-            var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_CONTAINER ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-            });
+            // // get target
+            var target = this.getNextEnergyStoringMechanism(creep);
             if(target) {
                 if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
