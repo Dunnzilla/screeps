77d93cf78fcbbc4ce184c1f991043dc75e7f7705
Jason Dunn <attnjd@gmail.com>
2017-09-10 15:24:28 -0500

    Move to Claim flag; is manual but whatever

--- prod/role.claimer.js
+++ prod/role.claimer.js
@@ -3,20 +3,17 @@ var cc = require('creep.choices');
 var roleClaimer = {
     /** @param {Creep} creep **/
     run: function(creep) {
+        if( ! Game.flags['Claim'] ) { return; }
 
-        var bestTarget = Game.getObjectById('5982fd1cb097071b4adbec43');
-        if(bestTarget) {
-            creep.say('⭐️');
-            var result = creep.claimController(bestTarget);
-            switch(result) {
-                case ERR_NOT_IN_RANGE: creep.moveTo(bestTarget, {visualizePathStyle: {stroke: '#0033FF'}}); break;
-                case OK: break;
-                default: console.log(`Cannot claim ${bestTarget}: ${result}`); break;
-            }
-        } else {
-             creep.say('🤔');
+        var bestTarget = Game.flags['Claim'];
+
+        creep.say('⭐️');
+        var result = creep.claimController(bestTarget);
+        switch(result) {
+            case ERR_NOT_IN_RANGE: creep.moveTo(bestTarget, {visualizePathStyle: {stroke: '#0033FF'}}); break;
+            case OK: break;
+            default: console.log(`Cannot claim ${bestTarget}: ${result}`); break;
         }
-
     }
 };
 
