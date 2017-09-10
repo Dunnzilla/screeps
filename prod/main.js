09083c08af3f9f75645d325bb75db86cc2fa919a
Jason Dunn <attnjd@gmail.com>
2017-09-10 15:52:02 -0500

    Three five-year olds at my house

--- prod/main.js
+++ prod/main.js
@@ -22,38 +22,41 @@ module.exports.loop = function () {
         shooter: 1,
         claimer: 0
     };
+    Game.rooms.forEach(function workRoom(room) {
+      var spawns = room.find(FIND_MY_SPAWNS);
+      spawns.forEach(function workSpawn(spawn) {
+        spawnThinker.cull(spawn.name, populationLimits);
+        spawnThinker.create(spawn.name, populationLimits);
+        towerBasic.run();
+        if(spawn.spawning) {
+            var spawningCreep = Game.creeps[spawn.spawning.name];
+            room.visual.text(
+                '🛠️' + spawningCreep.memory.role,
+                spawn.pos.x + 1,
+                spawn.pos.y,
+                {align: 'left', opacity: 0.8});
+        }
 
-    spawnThinker.cull('Spawn1', populationLimits);
-    spawnThinker.create('Spawn1', populationLimits);
+        const pctEnergy = (room.energyAvailable / room.energyCapacityAvailable) * 100.0;
+
+        for(var creep of room.find(FIND_MY_CREEPS)) {
+
+            if(spawnThinker.getPopulationPercent(populationLimits) < 50.0 && pctEnergy < 75.0) {
+                roleHarvester.runOptimized(creep);
+            } else {
+                switch(creep.memory.role) {
+                    case 'claimer': roleClaimer.run(creep); break;
+                    case 'harvester': roleHarvester.runOptimized(creep); break;
+                    case 'upgrader': roleUpgrader.run(creep); break;
+                    case 'builder': roleBuilder.run(creep); break;
+                    case 'repairman': roleRepairMan.run(creep); break;
+                    case 'shooter': roleShooter.run(creep); break;
+                }
+            }
+
+      });
+    });
 
     // require('spawn.thinker').create('Spawn1', {harvester: 5})
-    towerBasic.run();
-    if(Game.spawns['Spawn1'].spawning) {
-        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-        Game.spawns['Spawn1'].room.visual.text(
-            '🛠️' + spawningCreep.memory.role,
-            Game.spawns['Spawn1'].pos.x + 1,
-            Game.spawns['Spawn1'].pos.y,
-            {align: 'left', opacity: 0.8});
-    }
-
-
-    const pctEnergy = (Game.rooms['W22S35'].energyAvailable / Game.rooms['W22S35'].energyCapacityAvailable) * 100.0;
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-
-        if(spawnThinker.getPopulationPercent(populationLimits) < 50.0 && pctEnergy < 75.0) {
-            roleHarvester.runOptimized(creep);
-        } else {
-            switch(creep.memory.role) {
-                case 'claimer': roleClaimer.run(creep); break;
-                case 'harvester': roleHarvester.runOptimized(creep); break;
-                case 'upgrader': roleUpgrader.run(creep); break;
-                case 'builder': roleBuilder.run(creep); break;
-                case 'repairman': roleRepairMan.run(creep); break;
-                case 'shooter': roleShooter.run(creep); break;
-            }
-        }
     }
 }
